5cdc224856776d484b3f2827e7dc825533af8375
Denise Perry <dbperry2@asu.edu>
2018-04-04 12:53:34 -0700

    Initial Commit

---
+++ CSE460_7702087/Java/SubscriptionSystem/src/Subscription.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Subscription {
+
+	private String forum;
+
+	private ArrayList subscribers;
+
+	public boolean addSub(String user) {
+		return false;
+	}
+
+	public boolean removeSub(String user) {
+		return false;
+	}
+
+}
