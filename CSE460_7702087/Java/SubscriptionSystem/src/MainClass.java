cc60994c42d474e6a352ac04b76a490a65a26f5e
Denise Perry <dbperry2@asu.edu>
2018-04-13 18:41:57 -0700

    Regexes done

--- CSE460_7702087/Java/SubscriptionSystem/src/MainClass.java
+++ CSE460_7702087/Java/SubscriptionSystem/src/MainClass.java
@@ -4,17 +4,40 @@ public class MainClass
 {
 
 	private static boolean firstEvent;
+
 	private static Scanner kb;
+
+	private static Publisher[] publishers;
 
 	public static void main(String[] args)
 	{
+		//Begin
 		kb = new Scanner(System.in);
 		String command;
 		
+		/*
+		 * Commands:
+		 *		new forum,[forum name]
+		 *		new post,[user name],[forum name],[thread title],[post text]
+		 *		subscribe,[user name],[forum name]
+		 *		unsubscribe,[user name],[forum name]
+		 */
+		
 		while (!(command = kb.nextLine()).isEmpty())
 		{
-			System.out.println(command);
+			if(command.matches("new forum,[\\S ]+"))
+				System.out.println("New forum created!");
+			else if(command.matches("new post,[\\S ]+,[\\S ]+,[\\S ]+,[\\S ]+"))
+				System.out.println("New post created");
+			else if(command.matches("subscribe,[\\S ]+,[\\S ]+"))
+				System.out.println("User Subscribed");
+			else if(command.matches("unsubscribe,[\\S ]+,[\\S ]+"))
+				System.out.println("User Unsubscribed");
+			else
+				System.out.println("Command not recognized");
+				
 		}
+		//End
 	}
 
 }
